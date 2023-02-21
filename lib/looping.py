b6014848535adbedae16d1feb7a8c4df6706d1d3
Areeb Karim <areebakarim99@gmail.com>
2023-02-21 15:34:28 -0500

    done

--- lib/looping.py
+++ lib/looping.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 
+#   Write a method `happy_new_year` that outputs numbers starting at 10 and
+#    counting down to 1. After reaching 1, print out "Happy New Year!"
+
 def happy_new_year():
     for i in range(10, 0, -1):
         print(i)
-    
-    print("Happy New Year!")
+    if i == 1:
+        print("Happy New Year!")
+
+#   Write a method `fizzbuzz_printer` that prints the numbers from 1 to 100. For
+#   multiples of three, print "Fizz" instead of the number and for the multiples
+#   of five print "Buzz". For numbers which are multiples of both three and five,
+#   print "FizzBuzz".
+
+def fizzbuzz_printer():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
 
 def square_integers(int_list):
     return [i ** 2 for i in int_list]
@@ -19,3 +39,10 @@ def fizzbuzz():
             print("Fizz")
         else:
             print(i)
+
+#   Write a method `reverse_string` that takes one argument, a string, and reverses
+#   it. Don't use the built-in `.reverse` method. Instead, loop through the
+#   characters in the input string and reverse it.
+
+def reverse_string(string):
+    
